3ac6d19bd843bcf0ea13e1c1ca09736aebcc7dd2
LukaszZietek <lukaszzietekk@gmail.com>
2020-06-03 17:27:39 +0200

    Few changes in view

--- Repository/Models/Advertisement.cs
+++ Repository/Models/Advertisement.cs
@@ -15,27 +15,32 @@ namespace Repository.Models
 
         [Required]
         [MaxLength(50, ErrorMessage = "Za długi tytuł ogłoszenia")]
+        [Display(Name = "Tytuł")]
         public string Title { get; set; }
 
         [Required]
         [MinLength(150, ErrorMessage = "Za krótki opis ogłoszenia")]
+        [Display(Name="Opis")]
         public string Content { get; set; }
 
         [Required]
         [DataType(DataType.DateTime)]
+        [Display(Name="Data Dodania")]
         public DateTime AddTime { get; set; }
 
         [Required]
+        [Display(Name="Cena")]
         public double Price { get; set; }
 
         [Required]
         [Description("Zdjęcie")]
+        [Display(Name = "Zdjęcie")]
         public byte[] Image { get; set; }
 
         public string InternalUserId { get; set; }
 
         [Required]
-        [Display(Name = "Kategorie")]
+        [Display(Name = "Kategoria")]
         public int CategoriesId { get; set; }
 
 
